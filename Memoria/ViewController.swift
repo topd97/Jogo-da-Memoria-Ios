ec0fafc2e0de425ae4df24392ae8eaa4358b448d
Thiago Damasceno <thiago.damasceno@corp.globo.com>
2019-07-09 15:20:26 -0300

    Initial Commit

---
+++ Memoria/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memoria
+//
+//  Created by Thiago Outeiro Pereira Damasceno on 09/07/19.
+//  Copyright © 2019 Thiago Outeiro Pereira Damasceno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
